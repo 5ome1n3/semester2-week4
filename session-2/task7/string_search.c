792b8504654ec5be1babad6b00d23c8a0b3fec32
5ome1n3 <139634696+5ome1n3@users.noreply.github.com>
2026-02-17 10:48:28 +0000

    w4s2

--- session-2/task7/string_search.c
+++ session-2/task7/string_search.c
@@ -8,15 +8,49 @@
  * Hint: string library functions strcat() and strcspn() can be used
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+struct String {
+    char* arr;
+    int size;
+    int capacity;
+};
+void resize(struct String* str) {
+    char* temp = str->arr;
+    str->arr = malloc(str->capacity*sizeof(char)+10);
+    memcpy(&str->arr, temp, str->size*sizeof(char));
+    str->capacity +=10;
+    free(temp);
+}
+
+void append(struct String* str, const char* data) {
+    int dataSize = strlen(data);
+    while (dataSize > str->capacity) {
+        resize(str);
+    }
+    memcpy(&str->arr[str->size], data, strlen(data)*sizeof(char));
+    str->size += dataSize;
+    str->arr[str->size] = '\0';
+}
+
+void strInit(struct String* str) {
+    str->arr = malloc(sizeof(char) * 2);
+    str->arr[0] = '\0';
+    str->size = 0;
+    str->capacity = 2*sizeof(char);
+}
+void printString(struct String* str) {
+    printf("%s\n", str->arr);
+}
 
 int main( int argc, char **argv ) {
 
-    char sentence[200]="";
+    struct String string;
+    strInit(&string);
+    append(&string, "dfhsdfesdfslewwerwerdfgvdfghsfhsfjfjghduyasdgbfyuasdgbfuoyasdgfuosadgfyasdgfudsygsdffe");
+    printString(&string);
 
-    /*
-    Your code
-    */
 
     return 0;
 }
--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -5,15 +5,22 @@ Student ID:
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main( int argc, char **argv ) {
-
     // define appropriate data to hold your answer
-
     // process the command-line data using appropriate string functions
-
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+    for (int i=1;i<argc;i++) {
+        if (i < argc-1) {
+            printf("%s-", argv[i]);
+        }
+        else {
+            printf("%s\n", argv[i]);
+        }
+        
+    }
+    //printf("%s\n",buffer); // use only this single print statement in your submitted code
 
     return 0;
 }
